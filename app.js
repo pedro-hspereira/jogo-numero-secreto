55b2694b27cf723dfd5e7ce9a0c58c4fa7036927
Pedro Henrique <pedro.hsp2710@gmail.com>
2025-05-23 16:59:36 -0300

    mudanças no titulo e gitignore

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
+    exibirTextoNaTela('p', `Descubra o número entre 1 e ${numeroLimite}`);
 }
 exibirMensagemInicial();
 
